3d6b5c6a6b321451fc3ed1b70e8a038523a09182
DmitriySemenov <dsemenov@prosave.dk>
2015-03-26 18:16:22 +0200

    added template

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 angular.module('Search', [])
-.directive('result', function(){
+.controller('searchCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.list = array;
+  $scope.url = 'http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&place_name=leeds';
+  $http({method: 'POST', url: $scope.url}).success(function(data) {
+      $scope.posts = data; // response data 
+    });
+}])
+.directive('result', function(){  
   return {
-    scope: {},
-    controller: function($scope, $element, $attrs, $transclude) {},
     restrict: 'E',
     templateUrl: '../template.html'
   };
 });
+var array = [
+  {
+    price: 20945,
+    street: "Somewhere",
+    town: "nowhere"
+  },
+  {
+    price: 20,
+    street: "Somewhere",
+    town: "nowhere"
+  }
+];
