4ace4576a78604a9bea30b8ecb3d589a1b46d5a2
disem2 <disem2@yandex.ru>
2015-04-23 17:40:33 +0300

    cleared

--- js/controllers/details-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('Search')
-.controller('detailsCtrl', function($http, $routeParams){
-  this.url = '../../base.json';
-  var current = this;
-  $http({method: 'GET', url: this.url}).success(function(data) {
-    current.curr = data.response.listings[$routeParams.id];
-  });
-});
